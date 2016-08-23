2b1fc5141b3924b1d3646452b14aa09c297c4ee6
Chef-Code <lonnieteter@yahoo.com>
2016-08-23 10:43:29 -0700

    this too

--- code-examples/JavaScript/xhr.js
+++ code-examples/JavaScript/xhr.js
@@ -1,7 +1,11 @@
+//EventTarget is an interface implemented by objects that can receive events and may have listeners for them
+var eventTargets = ["Element","Document","Window","XMLHttpRequest","AudioNode","AudioContext"];
+var eventTargetMethods = ["addEventListener()","removeEventListener()","dispatchEvent()"];
+
 var readyStateArray = ["UNSENT", "OPENED", "HEADERS_RECEIVED", "LOADING", "DONE"];  //Will not work with IE Browser
 var IE_readyState = ["READYSTATE_UNINITIALIZED", "READYSTATE_LOADING", "READYSTATE_LOADED", "READYSTATE_INTERACTIVE","READYSTATE_COMPLETE"];
 
-var responseTypeValues = ["DOMString","ArrayBuffer","Blob","Document","json","text","moz-blob","moz-chunked-text","moz-chunked-arraybuffer","ms-stream"]
+var responseTypeValues = ["DOMString","ArrayBuffer","Blob","Document","json","text","moz-blob","moz-chunked-text","moz-chunked-arraybuffer","ms-stream"];
 function ajaxCall(method, url, response_type, async) {
 	 
 	let data = {};
